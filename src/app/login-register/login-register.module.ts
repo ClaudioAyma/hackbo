968e14115054da04e3e777ea6abe837f2f2d2afc
claudio <claudio.dev96@gmail.com>
2018-07-28 22:31:06 -0400

    modulo de registro y login

---
+++ src/app/login-register/login-register.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRegisterRoutingModule } from './login-register-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    LoginRegisterRoutingModule
+  ],
+  declarations: [LoginComponent, RegisterComponent]
+})
+export class LoginRegisterModule { }
---
+++ src/app/models/cliente.ts
@@ -0,0 +1,6 @@
+export interface cliente {
+    nombre: string,
+    ci: string,
+    telefono: string,
+    pasos: [{}]
+}
